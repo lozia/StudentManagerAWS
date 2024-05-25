821710c0e451339ae95febe5b9df0c8ed1edce6b
lozia <lizongyao9745@hotmail.com>
2024-05-25 07:53:31 -0700

    change api address

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -3,7 +3,8 @@ import { IStudent } from '../models/IStudent';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 
-const STUDENT_API = "http://3.12.152.242:8080/api/student"
+const STUDENT_API = "http://172.31.11.254:8080/api/student"
+
 
 @Injectable({
   providedIn: 'root'
